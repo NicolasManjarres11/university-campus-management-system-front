3ef3087941fae52d3d5cc1860026215351946b83
Manjarres <nicolasmanjarresg@gmail.com>
2025-08-05 21:45:02 -0500

    Se inicia con la logica del negocio en la autenticacion. Se dfinen propiedades con signal para validar el usuario actual, se define propiedad de solo lectura para ver la información del usuario actual, también se definen propiedades computadas que validarán si el usuario está logueado o validarán el rol. En el contructor se dejó el método que cargará el usuario en el localStorage, con el fin de que lo haga siempre se que utilice el servicio de autenticación. Se definió método de login, que buscará de la ´BD´ los usuarios guardados y hará la comparación de username, contraseña y permitirá el acceso del usuario o lo rechazará. Al momento de hacer login, se actualiza el signal del servicio con la información del usuario pero sin la contraseña, este también se guarda en el localStorage. Se defien tambi+en los métodos que validará el rol del usuario

--- src/app/core/services/auth-service.ts
+++ src/app/core/services/auth-service.ts
@@ -1,8 +1,130 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+import { User, UserRole } from '../../features/users/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  //Signal para el usuario actual
+  private currentUser = signal<User | null>(null);
+
+  //Signal de solo lectura 
+  public readonly user = this.currentUser.asReadonly();
+
+  //Validar si el usuario está logueado, se actualiza automáticamente
+  public isLoggedIn = computed(() => this.currentUser() !== null);
+
+  //Obtener rol de usuario
+  public userRole = computed(() => this.currentUser()?.role || null);
+
+  constructor(){
+    this.loadUserFromStorage();
+  }
+
+  //Métodos de autenticación
+
+  login(username: string, password: string): {success: boolean; message: string} {
+    try {
+      //Obtenemos los usuarios desde el localStorage
+
+      const storeUser = localStorage.getItem('users');
+
+      if(!storeUser){
+        return {
+          success: false,
+          message: 'No hay usuarios registrados'
+        }
+      }
+
+      //Guardamos los usuarios obtenidos en una lista de usuarios
+
+      const users: User[] = JSON.parse(storeUser);
+
+      //Buscamos el usuario en la base de datos
+
+      const user = users.find(u => u.username.toLowerCase() === username.toLowerCase());
+
+      if(!user){
+        return {
+          success: false,
+          message: 'Usuario no encontrado.'
+        }
+      }
+
+      if(user?.password !== password){
+        return {
+          success: false,
+          message: 'Contraseña incorrecta'
+        }
+      }
+
+      //Guardamos el usuario en el LocalStorage
+
+      const userWithoutPassword = {...user}; //Traemos todas las propiedades del usuario encontrado
+      delete userWithoutPassword.password; //Eliminamos la contraseña para guardar el usuario logueado en el localStorage, conservamos las demás propiedades
+
+  
+      localStorage.setItem('currenUser', JSON.stringify(userWithoutPassword)); //Guardamos el usuario en el localStorage
+      this.currentUser.set(userWithoutPassword); //Lo guardamos en la signal del servicio.
+
+      return {
+        success: true,
+        message: 'Se ha iniciado sesión exitosamente.'
+      }
+
+    } catch (error) {
+
+      console.error('Error durante el login: ', error);
+      return {
+        success: false,
+        message: 'Error interno del sistema.'
+      }
+      
+    }
+  }
+
+  logout(): void {
+
+
+  }
+
+  //Métodos de verificación de permisos
+  //Esto es para validar los permisos del usuario y con esto, poder mostrar u ocultar componentes o elementos de laágina según el rol que tengan.
+
+  hasRole(role : UserRole): boolean {
+
+    return this.userRole() === role;
+  }
+
+  isAdmin(role : string): boolean{
+
+    return this.userRole() === UserRole.ADMIN;
+
+  }
+  isProfessor(role : string): boolean{
+
+    return this.userRole() === UserRole.PROFESSOR;
+
+  }
+  isStudent(role : string): boolean{
+
+    return this.userRole() === UserRole.STUDENT;
+  }
+
+  private loadUserFromStorage(): void {
+    try {
+
+      const storeUser = localStorage.getItem('currentUser'); //Obtenemos del localStorage el item currenUser
+      if(storeUser){
+        const user =JSON.parse(storeUser); //Si hay, lo pasamos de string a JSON
+        this.currentUser.set(user); //Actualizamos la Signal con el usuario cargado , //Las Signal computadas se activan
+      }
+      
+    } catch (error) {
+      console.error('Error cargando el usuario desde el storage: ', error);
+      localStorage.removeItem('currentUser')
+    }
+  }
   
 }
