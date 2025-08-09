f6990a551e456a0a986cd603a989f1c3bdc5f1bc
Manjarres <nicolasmanjarresg@gmail.com>
2025-08-09 18:00:49 -0500

    borrador de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,59 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+/*     { path: '', pathMatch: 'full', redirectTo: 'courses' }, */
+
+/*     // Login público (solo invitados)
+    {
+      path: 'login',
+      // Ajusta al componente real que uses de login
+      loadComponent: () => import('@features/users/components/login/login.component')
+        .then(m => m.LoginComponent),
+      canMatch: [guestOnlyGuard]
+    },
+  
+    // Users (solo Admin)
+    {
+      path: 'users',
+      loadChildren: () => import('@features/users/users.routes').then(r => r.routes),
+      canMatch: [roleGuard],
+      data: { roles: [UserRole.ADMIN] }
+    },
+  
+    // Courses (todos los roles)
+    {
+      path: 'courses',
+      loadChildren: () => import('@features/courses/courses.routes').then(r => r.routes),
+      canMatch: [roleGuard],
+      data: { roles: [UserRole.ADMIN, UserRole.PROFESSOR, UserRole.STUDENT] }
+    },
+  
+    // Departments (solo Admin)
+    {
+      path: 'departments',
+      loadChildren: () => import('@features/departments/departments.routes').then(r => r.routes),
+      canMatch: [roleGuard],
+      data: { roles: [UserRole.ADMIN] }
+    },
+  
+    // Schedules (todos los roles)
+    {
+      path: 'schedules',
+      loadChildren: () => import('@features/schedules/schedules.routes').then(r => r.routes),
+      canMatch: [roleGuard],
+      data: { roles: [UserRole.ADMIN, UserRole.PROFESSOR, UserRole.STUDENT] }
+    },
+  
+    // Grades (todos los roles; la restricción fina va en el servicio/UI)
+    {
+      path: 'grades',
+      loadChildren: () => import('@features/grades/grades.routes').then(r => r.routes),
+      canMatch: [roleGuard],
+      data: { roles: [UserRole.ADMIN, UserRole.PROFESSOR, UserRole.STUDENT] }
+    },
+  
+    { path: '**', redirectTo: '' } */
+
+
+];
